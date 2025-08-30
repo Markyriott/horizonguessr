378a1896aa882d5db60cd1d2840c86175db21e22
Mark <208412620+Markyriott@users.noreply.github.com>
2025-08-29 23:14:06 -0400

    barebones machineguessr working

--- src/components/games/MachineGuessr.jsx
+++ src/components/games/MachineGuessr.jsx
@@ -1,15 +1,107 @@
-import React, { useState } from "react";
+import React, { useState, useCallback,useEffect } from "react";
+import { machinesdata } from "../../data/machine_data";
 
-function Choice({ text, handleClick}){
+const MACHINES = Object.keys(machinesdata).map(key =>({name:key}));
+
+function Choice({ text, handleClick }){
     return(
         <button className="choice" onClick={handleClick}> {text} </button>
     )
 }
 
-export default function UnlimitedMachineGuessr({}){
+function PlayArea({ answer, options, answerCheck, difficulty}){
+    const handleClick = (selectedOption)=>{
+        if (answer == selectedOption){
+            answerCheck(true);
+        } else {answerCheck(false)}
+    }
+
+    const handleSubmit = (inputMachine) =>{
+        if (answer.toLowerCase() == inputMachine.trim().toLowerCase()){
+            answerCheck(true);
+        } else {answerCheck(false)}
+    }
+    return (
+        <div className='play-area'>
+            {difficulty <= 1 ?
+            options.map((option)=>(
+                <Choice
+                    key={option.name}
+                    text={option.name}
+                    handleClick={()=>handleClick(option.name)}
+                />
+            )) : <p>Diff greater than 1</p>}
+        </div>
+    )
+}
+
+export default function UnlimitedMachineGuessr({  }){
+    const [currentMachine, setCurrentMachine] = useState([]);
+    const [options, setOptions] = useState([]);
+    const [score, setScore] = useState(0);
+    const [health, setHealth] = useState(3);
+
+    useEffect(()=>{
+        getQuestion();
+    }, [])
+    
+    const getQuestion = useCallback(() => {
+        const question = generateQuestion(MACHINES, 3, currentMachine);
+        setCurrentMachine(question[0]);
+        setOptions(question[1]);
+    }, [currentMachine, score])
+
+
+    const resetGame = () =>{
+        setScore(0);
+        setHealth(3);
+        getQuestion();
+    }
+    const handleAnswer = (answer) => {
+        if (answer){
+            console.log("YOU WON! GENERATING NEXT QUESTION!");
+            setScore(score + 1);
+            getQuestion();
+        } else {
+            if (health > 1){
+                setHealth(health - 1);
+                console.log(`current health: ${health - 1}`);
+            } else {
+                console.log('lost... resetting...');
+                resetGame();
+            }
+        }
+    }
+
+
     return(
         <div className="game-container">
-            <Choice />            
+            <h2>{currentMachine.name}</h2>
+            <PlayArea answer = {currentMachine.name} options = {options} answerCheck = {handleAnswer} difficulty={0} />
         </div>
     )    
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function generateQuestion(arr, numOptions, lastOption){
+  const shuffled = shuffleArray([...arr]);
+
+  const getNextOption = () => shuffled[shuffled.length - 1] === lastOption ? shuffled[shuffled.length - 2] : shuffled[shuffled.length - 1];
+  const nextOption = getNextOption();
+
+  const specificIndex = shuffled.findIndex((item) => item.name === nextOption.name);
+
+  if (specificIndex >= numOptions + 1) {
+      const randomIndex = Math.floor(Math.random() * (numOptions + 1));
+      [shuffled[specificIndex], shuffled[randomIndex]] = [shuffled[randomIndex], shuffled[specificIndex]];
+  }
+  const options = shuffled.slice(0, numOptions + 1);
+  return [nextOption, options];
 }
---
+++ src/data/machine_data.jsx
@@ -0,0 +1,63 @@
+// type Machine = {
+//     id: string,
+//     name: string,
+//     game: string,
+//     imagepath: string,
+//     class: string
+// };
+
+// type Machinesdata = {
+//     [key: string]: Machine
+// };
+
+export const machinesdata = { //: Machinesdata = {
+    'Watcher': {
+        id: '1',
+        name: "Watcher", 
+        game: "Horizon Zero Dawn",
+        imagepath: "/machines/1",
+        class: "Recon"
+    },
+    'Scrapper': {
+        id: '2',
+        name: "Scrapper", 
+        game: "Horizon Zero Dawn",
+        imagepath: "/machines/2",
+        class: "Acquisition"
+    },
+    'Strider': {
+        id: '3',
+        name: "Strider", 
+        game: "Horizon Zero Dawn",
+        imagepath: "/machines/3",
+        class: "Acquisition"
+    },
+    'Charger': {
+        id: '4',
+        name: "Charger", 
+        game: "Horizon Zero Dawn",
+        imagepath: "/machines/4",
+        class: "Acquisition"
+    },
+    'Grazer': {
+        id: '5',
+        name: "Grazer", 
+        game: "Horizon Zero Dawn",
+        imagepath: "/machines/5",
+        class: "Acquisition"
+    },
+    'Sawtooth': {
+        id: '6',
+        name: "Sawtooth", 
+        game: "Horizon Zero Dawn",
+        imagepath: "/machines/6",
+        class: "Combat"
+    },
+    'Tallneck':{
+        id:'7',
+        name:"Tallneck",
+        game:"Horizon Zero Dawn",
+        imagePath: "",
+        class: ""
+    }
+}
